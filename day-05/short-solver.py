8e306353dd5db6d7fa6725341f136373cb7e8ff7
Sabbir Muhit <sabbir.muhit@gartner.com>
2020-12-05 13:08:17 -0500

    Day 5 - Made the short solver even shorter

--- day-05/short-solver.py
+++ day-05/short-solver.py
@@ -1,6 +1,6 @@
 # After solving, realized that I could get the code much shorter
 
-data = set([int(x.replace('F', '0').replace('L', '0').replace('B', '1').replace('R', '1'), base=2) 
+data = set([int(x.translate(str.maketrans('FBLR', '0101')), base=2)
             for x in open('input.txt').read().split()])
 
 print('Part 1', max(data))
